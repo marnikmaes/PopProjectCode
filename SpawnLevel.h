5abd0f6b2b3856049b7b2febe4c2305fb13a1e7b
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2022-05-01 11:05:26 +0200

    SpawnLevel.h

---
+++ SpawnLevel.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnLevel.generated.h"
+
+class ABaseLevel;
+
+UCLASS()
+class ENDLESSRUNNERGAME_API ASpawnLevel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnLevel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+
+	//check if our spawnlevel is the first level we want to spawn into the game
+	UFUNCTION()
+		void SpawnLevel(bool IsFirst);
+	
+	UFUNCTION()
+		void OnOverlapBegin(UPrimitiveComponent* OverlappedComp,
+			AActor* OtherActor, UPrimitiveComponent* OtherComp,
+			int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+protected:
+	APawn* Player;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<ABaseLevel>Level1;
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<ABaseLevel>Level2;
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<ABaseLevel>Level3;
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<ABaseLevel>Level4;
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<ABaseLevel>Level5;
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<ABaseLevel>Level6;
+
+	TArray<ABaseLevel*> LevelList;
+
+public:
+	int  RandomLevel;
+	FVector SpawnLocation = FVector();
+	FRotator SpawnRotation = FRotator();
+	FActorSpawnParameters SpawnInfo = FActorSpawnParameters();
+};
